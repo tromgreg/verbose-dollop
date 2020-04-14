3797208120ef128178c0071a4a50702bbee493c9
tromgreg <grelarson1@gmail.com>
2020-04-14 13:41:39 -0700

    trying to find how to use loops with my functions

--- pass.js
+++ pass.js
@@ -2,21 +2,21 @@
 
 
 
-
-
-
-
-
 //this variable adds the below functions together
-var randomFunc = {
+//var wasn't working and I had to change it to const instead.
+//https://medium.com/@josephcardillo/the-difference-between-function-and-block-scope-in-javascript-4296b2322abe
+//good article for know the difference between var, let, const and understanding blocks.
+//var is function scope.
+//let and const are block scope.
+//Function scope is within the function.
+//Block scope is within curly brackets.
 
-    lower : getLowerCase,
-    upper : getUpperCase,
-    numbers : getNumbers,
-    symbol : getSpecial,
-
-}
-
+const randomFunc = {
+	lower: getLowerCase,
+	upper: getUpperCase,
+	number: getNumbers,
+	symbol: getRandomsymbols
+};
 
 
 //http://www.net-comber.com/charset.html
@@ -24,33 +24,36 @@ var randomFunc = {
 //26 letters of the alphabet and get math.random to make random whole number rounded down starting from charcode 97.
 
 function getLowerCase(){
-
-    return String.fromCharCode(math.floor(math.random() * 26) + 97);
-
+    //math.random get a random decimal number up to 26 and use math.floor to random down.
+    //from the 97th charcode and up 26 spots. 
+    return String.fromCharCode (Math.floor(Math.random() * 26) + 97);
 }
+console.log(getLowerCase());
 
+// almost the same as above.
 function getUpperCase(){
 
-    return String.fromCharCode(math.floor(math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
 }
- 
+
+console.log(getUpperCase());
+// about the same premise. since 0-9 there are 10 numbers which start in the 48th charCode.
 function getNumbers(){
 
-    return String.fromCharCode(math.floor(math.random() *  10) + 48);
+    return String.fromCharCode(Math.floor(Math.random() *  10) + 48);
 
 }
 
-function getSpecial(){
+console.log(getNumbers());
+//random symbols are only pulled from the const below
+function getRandomsymbols(){
     //variable with symbols
     const symbols = "!@#$%^&*()_+?{}[]<>"
     //return from variable array above
-    return symbols [math.floor(math.random() * symbols.length)];
-
+    return symbols [Math.floor(Math.random() * symbols.length)];
 }
-
-//add required dropdowns to control what I want my passwords's makeup to be.
-
+console.log(getRandomsymbols());
 
 
 
